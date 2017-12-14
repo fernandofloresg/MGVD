890f48c24225b25532a68623ff6374a0780baa4c
fernandofloresg <fflores10@uabc.edu.mx>
2017-12-14 02:19:31 -0700

    añadi proyecto en flask

---
+++ Proyeto/Servidor/hello.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask('Sensacionalismo')
+
+@app.route("/" )
+def hello():
+    return render_template('index.html', name='index_template')
+
+@app.route('/', methods=['POST'])
+def turn_on():
+    url = request.form['url']
+    os.system('python consulta.py ' + url)
+    return '', 204  # no content
+
+if __name__ == "__main__":
+    app.run()
